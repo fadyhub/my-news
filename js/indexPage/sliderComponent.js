693b64df11b09248a1766806df5cefeea6a825cf
fadyhub <PUT_YOUR_EMAIL_HERE>
2024-09-20 14:58:22 +0300

    first commit

---
+++ js/indexPage/sliderComponent.js
@@ -0,0 +1,9 @@
+class SliderComponent extends HTMLElement {
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+        this.innerHTML = `<h1>${this.getAttribute('text')}</h1>`
+    }
+}
+window.customElements.define('slider-component', SliderComponent)
